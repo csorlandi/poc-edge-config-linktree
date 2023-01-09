ee24460aa2f84cf7e69d817061d35b8cc9e3f3cb
Claudio Orlandi <claudiosorlandi@gmail.com>
2023-01-08 22:00:21 -0300

    feat(seo): add head metatags

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>My Page</title>
+      <meta
+        name="description"
+        content="A Linktree inspired app using NextJS, Tailwind CSS and Vercel Edge Config"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
